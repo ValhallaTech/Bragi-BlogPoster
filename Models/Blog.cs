790821f2ef302bd956eefa3bc9676014dd8cf468
Fred <valhallatechnc@gmail.com>
2020-10-20 14:33:51 -0400

    Initial commit

---
+++ Models/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPosts.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; set; }
+    }
+}
---
+++ Models/BlogUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlogPosts.Models
+{
+    public class BlogUser : IdentityUser
+    {
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string LastName { get;    set; }
+        public string DisplayName { get; set; }
+    }
+}
