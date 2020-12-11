bcdf6571992fee7f4f58e80568ff9116aee9a826
Fred <valhallatechnc@gmail.com>
2020-12-11 13:25:33 -0500

    Minor UI updates

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -49,10 +49,18 @@ namespace BragiBlogPoster.Areas.Identity.Pages.Account
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "FirstName")]
+            public string LastName { get; set; }
+
+            [Required]
+            [Display(Name = "LastName")]
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(60, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
